8fe3174a3d7044a2b718c736b8ca978ae213cfb4
Baptiste CHOCOT <baptiste.chocot@nist.gov>
2021-11-19 09:55:15 -0500

    Improve stopping/killing

---
+++ chocobot.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+require_relative 'lib/bot'
+
+# If PID file already exists, consider it is still running
+pid_file = 'chocobot.pid'
+if File.file?(pid_file)
+    $stderr.puts "Chocobot is already running (PID ##{File.read(pid_file.chomp)})"
+    exit 1
+end
+
+# Writing PID into a file
+File.open(pid_file, 'w') { |f| f.write "#{Process.pid}" }
+
+
+bot = ChocoBot.new
+bot.run
+
+# Removing PID file on exit
+File.delete(pid_file)
--- run.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-# frozen_string_literal: true
-
-require_relative 'lib/bot'
-
-bot = ChocoBot.new
-bot.run
